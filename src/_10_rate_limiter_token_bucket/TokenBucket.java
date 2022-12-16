dba7eab2aa940ea453968103c2470e919caaa03d
tharun kalwa <tharunk@tk-Mac.local>
2022-12-16 17:01:19 +0530

    fix currenttokens bug

--- src/_10_rate_limiter_token_bucket/TokenBucket.java
+++ src/_10_rate_limiter_token_bucket/TokenBucket.java
@@ -20,6 +20,7 @@ public class TokenBucket {
 //            (tokensToBeAdded > 0) condition is important. this makes sure we update only if we were able to add new tokens. or else,
 //            we may never ever add tokens to bucket due to truncation  but ending up updating only last updated time.
             currentTokens += tokensToBeAdded;
+            currentTokens = Math.min(currentTokens, this.bucketSize);
         }
         if (currentTokens > 0) {
             currentTokens--;
