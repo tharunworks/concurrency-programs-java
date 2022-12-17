e448d6029ca93897e845f1a1729035dd4ab07630
tharun kalwa <tharunk@tk-Mac.local>
2022-12-17 11:20:16 +0530

    minor change in token bucket

--- src/_10_rate_limiter_token_bucket/TokenBucket.java
+++ src/_10_rate_limiter_token_bucket/TokenBucket.java
@@ -15,13 +15,10 @@ public class TokenBucket {
 
     public synchronized boolean getAccess() {
         long currentTime = System.currentTimeMillis();
+//        note: first calculate the no.of time-units elapsed and then multiple it with refill rate
         int tokensToBeAdded = (int) (((currentTime - this.lastUpdateTime)  / 1000) * this.refillRate);
-        if (tokensToBeAdded > 0) {
-//            (tokensToBeAdded > 0) condition is important. this makes sure we update only if we were able to add new tokens. or else,
-//            we may never ever add tokens to bucket due to truncation  but ending up updating only last updated time.
-            currentTokens += tokensToBeAdded;
-            currentTokens = Math.min(currentTokens, this.bucketSize);
-        }
+        currentTokens += tokensToBeAdded;
+        currentTokens = Math.min(currentTokens, this.bucketSize);
         if (currentTokens > 0) {
             currentTokens--;
             return true;
